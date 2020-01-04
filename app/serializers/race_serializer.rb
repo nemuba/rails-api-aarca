e93352c4371c293a1dae755f8fa7ec3773728c5d
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-01-04 17:02:24 -0200

    change date_race for to_date

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,3 +1,7 @@
 class RaceSerializer < ActiveModel::Serializer
   attributes :id, :local, :description, :date_race, :created_at, :updated_at
+
+  def date_race
+    object.date_race.to_date
+  end
 end
