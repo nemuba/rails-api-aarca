2eb4e0492b984e4428f9ed84eede10248a8e7527
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-01-10 19:45:22 -0200

    change date_race of the model race

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -2,6 +2,6 @@ class RaceSerializer < ActiveModel::Serializer
   attributes :id, :local, :description, :date_race, :status, :created_at, :updated_at
 
   def date_race
-    object.date_race.to_date
+    object.date_race.strftime('%d/%m/%Y')
   end
 end
