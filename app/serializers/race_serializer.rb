cde8d9ecd3db135a0b45669db4584d5d05488892
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-01-10 20:22:01 -0200

    config cors

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,7 +1,7 @@
 class RaceSerializer < ActiveModel::Serializer
   attributes :id, :local, :description, :date_race, :status, :created_at, :updated_at
 
-  def self.date_race
+  def date_race
     object.date_race.strftime('%d/%m/%Y')
   end
 end
