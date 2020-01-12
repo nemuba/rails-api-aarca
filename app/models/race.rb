7068e09fd2c11fa10e512dfa5aa599e9d7aea544
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-01-12 10:36:26 -0200

    fix error validate status

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,7 +3,7 @@ class Race < ApplicationRecord
   paginates_per 5
   has_many :tests, dependent: :destroy
 
-  validates :local, :description, :date_race, presence: true
+  validates :local, :description, :date_race, :status presence: true
   validates :local, :description, length: { minimum: 5 }
 
   enum status: {:closed => 0, :open => 1, :finish => 2}
