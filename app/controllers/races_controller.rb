83b4930303bc5fada980350d7fdec92350984a20
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-02-10 22:22:59 -0200

    resolve bug

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -30,7 +30,7 @@ class RacesController < ApplicationController
           local: @race.local,
           description: @race.description,
           status: @race.status,
-          sponsor: @race.sponsor.name
+          sponsor: @race.sponsor.name,
           date_race: @race.date_race.strftime('%Y-%m-%d')
       }.to_json
     render json: json
