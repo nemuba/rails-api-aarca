0f727b3519f30eaf0136a79da65947e78cbccf43
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-02-10 22:29:43 -0200

    resolve bug race index

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -13,7 +13,7 @@ class RacesController < ApplicationController
                     local: race.local,
                     description:  race.description,
                     status: race.status,
-                    sponsor: race.sponsor.name,
+                    sponsor: !race.sponsor.nil? ? race.sponsor.name : '',
                     date_race: race.date_race.strftime('%Y-%m-%d')
                   }
                   end
