9ff04b35717aa6a4d44ada57837e5b90b1bcc5e8
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-02-17 09:20:37 -0300

    configurando o serializer race

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,5 +1,5 @@
 class RaceSerializer < ActiveModel::Serializer
-  attributes :id, :local, :description, :date_race, :status, :sponsor_id,  :created_at, :updated_at
+  attributes :id, :local, :description, :date_race, :status, :sponsor_id
 
   def date_race
     object.date_race.strftime('%d/%m/%Y')
